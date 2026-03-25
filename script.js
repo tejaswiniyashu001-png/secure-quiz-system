833228a94846b10c8cc8542f9e7dab58706bbe14
tejaswiniyashu001-png <tejaswiniyashu001@gmail.com>
2026-03-25 22:28:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Result</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+<h2>Your Quiz Result</h2>
+<h1 id="score"></h1>
+<button onclick="logout()">Logout</button>
+</div>
+<script src="script.js"></script>
+</body>
+</html>
